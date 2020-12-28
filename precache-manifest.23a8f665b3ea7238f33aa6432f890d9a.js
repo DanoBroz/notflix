f8f697bf6c0a993990b0d1dd97fc244a3f5a134b
Daniel Brož <danobroz66@gmail.com>
2020-12-28 13:21:36 +0100

    Updates

---
+++ precache-manifest.23a8f665b3ea7238f33aa6432f890d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adc822c656696304e0d67da4704225e",
+    "url": "/notflix/index.html"
+  },
+  {
+    "revision": "b87fe5535a7c03361057",
+    "url": "/notflix/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b87fe5535a7c03361057",
+    "url": "/notflix/static/js/2.6a38f3d2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/notflix/static/js/2.6a38f3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220a48e5fb3323aee117",
+    "url": "/notflix/static/js/main.44432117.chunk.js"
+  },
+  {
+    "revision": "398eef7ed9c6ccce02c6",
+    "url": "/notflix/static/js/runtime-main.bbf010ab.js"
+  }
+]);
